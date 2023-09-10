a873b456fdbf8fd19e6ab50230a59d8647843810
JaDoggx86 <JaDogg@users.noreply.github.com>
2023-09-10 13:26:33 +0100

    add ability to load state as multiple small files

---
+++ make-yaksha.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf images ; mkdir images
+cd tools/docker/debian
+./build-container.sh
+./build-state.js
+cd ../../../images
+rm debian-9p-rootfs.tar   
+zstd debian-state-base.bin && rm debian-state-base.bin 
+mv debian-state-base.bin.zst debian-state-base.bin
+cd ../yaksha-playground
+rm -rf images
+cp -r ../images .
+rm -rf state ; mkdir state
+mv images/debian-state-base.bin ./state
+cd state
+split --bytes=12M debian-state-base.bin
+rm debian-state-base.bin
